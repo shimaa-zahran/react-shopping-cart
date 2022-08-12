25043f7e0972e90392c762aceed7465d8db3c188
shimaa-zahran <shimoaazahran@gmail.com>
2022-08-13 01:15:48 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Header from "./componants/Header/Header";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="layout">
+      <Header> </Header>
+      <main>content</main>
+      <footer>footer</footer>
+    </div>
+  );
 }
 
 export default App;
